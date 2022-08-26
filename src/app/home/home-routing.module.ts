3c38589468b14a6269dd3ad9e072dc2069819d07
Angu017 <101602981+Angu017@users.noreply.github.com>
2022-08-26 18:47:47 -0400

    aaaaa

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { Comp1Component } from '../components/comp1/comp1.component';
+import { Comp2Component } from '../components/comp2/comp2.component';
+import { Comp3Component } from '../components/comp3/comp3.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path:'comp1',
+        component: Comp1Component
+      },
+      {
+        path:'comp2',
+        component: Comp2Component
+      },
+      {
+        path: 'comp3',
+        component: Comp3Component
+      }
+    ]
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,32 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-import { RouterModule, Routes } from '@angular/router';
 import { HomePageRoutingModule } from './home-routing.module';
 import { Comp1Component } from '../components/comp1/comp1.component';
 import { Comp2Component } from '../components/comp2/comp2.component';
 import { Comp3Component } from '../components/comp3/comp3.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomePage,
-    children: [
-      {
-        path:'inicio',
-        component: Comp1Component
-      },
-      {
-        path:'registroAU',
-        component: Comp2Component
-      },
-      {
-        path: 'registroPE',
-        component: Comp3Component
-      }
-    ]
-  }
-];
+
 
 @NgModule({
   imports: [
@@ -37,6 +17,6 @@ const routes: Routes = [
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,Comp1Component,Comp2Component,Comp3Component]
 })
 export class HomePageModule {}
